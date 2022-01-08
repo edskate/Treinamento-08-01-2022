056f9bdce3bac21f2d8ec14c4c492666d1ade0b4
Edilson Gafanhoto <68605911+edskate@users.noreply.github.com>
2022-01-08 02:28:39 -0300

    Console.log

---
+++ To-do List Inglês/app.js
@@ -0,0 +1,12 @@
+console.console.log(Date);
+console.console.log(const name = new type(arguments););
+console.console.log(name);
+console.console.log(export {  } from 'module');
+console.useLayoutEffect((effect) => {
+              effect
+              return (const) => {
+                            cleanup
+              };
+}, [input])
+const head = arr => arr[0//#region ];
+console.log()
